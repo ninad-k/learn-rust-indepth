a2e57d0927e7b87cddf2b2d05f5e7c519344eaa7
Ninad K <ninadsanjaykulkarni@gmail.com>
2023-10-11 07:27:09 +0400

    Initial Commit

---
+++ lets-get-rusty/hello-world/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+
+
+    // Creation
+    let a: i16 = 5;
+    //mutability
+    //a = 10;
+    // shadowing
+
+    // scope
+
+    println!("End, world! {}", a);
+}
